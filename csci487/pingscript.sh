93138065974b3a053a852705cc3d80764dbc9ff7
grantHaataja <grant.haataja@gmail.com>
2019-01-29 15:25:11 -0600

    Added csci487 directory and pingscript.sh bash script

---
+++ csci487/pingscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ "$1" == "" ] 
+then
+echo "Usage: ./pginscript.sh [network]"
+echo "example: ./pingscript.sh 192.168.0"
+else
+for x in `seq 1 254`; do
+ping -c 1 $1.$x | grep "64 bytes" | cut -d" " -f4 | sed 's/.$//' | tee ip.list
+done
+fi
+
