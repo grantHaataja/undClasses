f09169b5429842821ae239db0409edbacc7f1a4a
grantHaataja <grant.haataja@gmail.com>
2019-01-29 15:40:37 -0600

    fixed bash script

--- csci487/pingscript.sh
+++ csci487/pingscript.sh
@@ -5,7 +5,7 @@ echo "Usage: ./pginscript.sh [network]"
 echo "example: ./pingscript.sh 192.168.0"
 else
 for x in `seq 1 254`; do
-ping -c 1 $1.$x | grep "64 bytes" | cut -d" " -f4 | sed 's/.$//' | tee ip.list
+ping -c 1 $1.$x | grep "64 bytes" | cut -d" " -f4 | sed 's/.$//' | tee -a ip.list
 done
 fi
 
