4ab4520fcd1abedaaddaa996c176a4f38d9d0e4a
Grant Haataja <grant.haataja@gmail.com>
2019-02-06 14:13:34 -0600

    Did work

---
+++ csci230/hw4-GrantHaataja.c
@@ -0,0 +1,29 @@
+//Grant Haataja
+//Assignment 4
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//vector function
+char * vector(char *characters) {
+	
+
+}
+
+//Main Function
+int main(void) {
+	char userInput = ' ';
+	char *characters;
+	char *hw4;
+	hw4 = "HW 4 input: ";
+	//prompt user for input
+	printf("Enter characters: ");
+	do {
+		//get character one at a time
+	       	userInput = getchar();
+
+
+	} while (userInput != '\n');
+
+
+}
