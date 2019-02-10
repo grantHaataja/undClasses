08d10b09a8ac0fb5c2ad16b2d8b6792f05d464e9
Grant Haataja <grant.haataja@und.edu>
2019-02-10 13:56:46 -0600

    Just for you

--- csci230/hw4-GrantHaataja.c
+++ csci230/hw4-GrantHaataja.c
@@ -8,8 +8,8 @@
 //vector function
 char * vector(char *characters) {
 	//allocate memory for array characters
-	int count = strlen(characters);
-	printf("\n%d", count); //FIXME delete
+	int count = strlen(characters) + 1;
+	printf("\n%d\n", count); //FIXME delete
 	characters = calloc(count, sizeof(char));
 
 }
@@ -20,20 +20,23 @@ int main(void) {
 	char *characters;
 	char *hw4;
 	hw4 = "HW 4 input: ";
+	characters = "";
+	userInput = calloc(100, sizeof(char));
 	//prompt user for input
 	printf("Enter characters: ");
-	vector(characters);
-	/*while (1); {
-		printf("REALLY?"); //FIXME delete
-		//get character one at a time
-	    //userInput[0] = getchar();
-	    printf("%s", characters); //FIXME delete
-	    printf("WHADDAFUQ"); //FIXME delete
+	int i = 0;
+	printf("\nDick\n"); //FIXME delete
+	while (userInput[i] != '\n'); {
+    	//get character one at a time
+		userInput[i] = getchar();	    
+	    printf("\n%s\n", characters); //FIXME delete
+	    printf("WHADDAFUQ\n"); //FIXME delete
 		//call vector function to allocate memory
 		vector(characters);
 		//add the user-entered char to string charaters
-		strcat(userInput, characters);
-	}*/
+		strcat(userInput[i], characters);
+		i++;
+	}
 	
 	printf("%s", characters); //FIXME delete
 	
