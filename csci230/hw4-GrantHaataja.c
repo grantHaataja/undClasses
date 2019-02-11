0678e5dbc5ac8768670f8311efb65f66ba8fc250
Grant Haataja <grant.haataja@und.edu>
2019-02-11 17:43:35 -0600

    fixed up

--- csci230/hw4-GrantHaataja.c
+++ csci230/hw4-GrantHaataja.c
@@ -7,20 +7,16 @@
 
 //vector function
 char * vector(char *characters, int count) {
+	char * ptr;
 	//allocate memory for any string
-	//int count = strlen(characters) + 1;
-	printf("\nI'm in the function, about to allocate %d spaces of memory...\n", count); //FIXME delete
-	
-	characters = (char*) calloc(count, sizeof(char));
-	printf("I'm in the function, just finished allocating %d spaces of memory...\n", count); //FIXME delete
-	
-	printf("\nI'm about to return %s.\n", characters); //FIXME delete
-	return characters;//temp sring for userInput
+	ptr = (char*) calloc(count+1, sizeof(char));
+	//copy memory into ptr to return to Main
+	memcpy(ptr, characters, count);
+	return ptr;
 }
 
 //Main Function
 int main(void) {
-	char *userInput;
 	char *characters;
 	char *hw4;
 	char *temp;
@@ -32,39 +28,25 @@ int main(void) {
 	strcpy(temp, hw4);
 	//initialize characters to an empty string
 	characters = "";
-	//initialize userInput to an empty string
-	userInput = "";
 	printf("Enter characters: ");
 	int i = 0;
-	//allocate memory for userInput
-	//userInput = vector(userInput);
-	userInput = calloc(100, sizeof(char)); //FIXME delete
-	printf("\nuserInput has %d spaces of memory...\n", strlen(userInput)); //FIXME delete
+	//allocate memory for characters
+	characters = calloc(2, sizeof(char)); 
 	while (1) {
-		printf("\nITERATION %d\n", i); //FIXME delete
 		//get character one at a time
-		userInput[i] = getchar();
-		printf("\nMy userInput is %s\n", userInput); //FIXME delete    	
+		characters[i] = getchar();		  	
 		//check to break
-		if (userInput[i] == '\n') {
+		if (characters[i] == '\n') {
 			break;
 		}	
 		//call vector function to allocate memory
 		characters = vector(characters, count);
-		printf("\nI'm in the loop, just finished allocating memory\n"); //FIXME delete
-		//add the user-entered char to string characters
-		printf("\nArray (characters) has %d spaces of memory...\n", count); //FIXME delete
-		printf("\nI'm about to cat %s onto my character array which is: %s\n", userInput, characters); //FIXME delete
-		characters = strcat(characters, userInput);
-		printf("\nArray (characters) is: %s.\n", characters); //FIXME delete
 		//count how many characters are in array "characters" 
 		count = strlen(characters) + 1;
 		i++;
 	}
-	printf("\nI finished the loop!\n"); //FIXME delete
 	//allocate memory and append characters to hw4
-	hw4 = (char*) malloc((strlen(hw4) + strlen(characters) + 2) * sizeof(char));
-	printf("\nHW4 String has %d characters in it...\n", strlen(hw4) + strlen(characters) + 2); //FIXME delete
+	hw4 = (char*) malloc((strlen(hw4) + count + 2) * sizeof(char));
 	//cat the temp string to hw4 after the allocation of memory wiped it clean
 	strcat(hw4, temp);
 	//free memory for temp string
@@ -75,11 +57,9 @@ int main(void) {
 	free(characters);
 	//print hw4 as one final string to the terminal screen
 	printf("\n%s\n", hw4);
-	
 	//free memory
-	//free(userInput);
-	//free(hw4);
-
+	free(hw4);
 	
 	return 0;
 }
+
