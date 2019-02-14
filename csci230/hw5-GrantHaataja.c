83f57e1fb746c8e473b971f362c366fbc6f579a8
Grant Haataja <grant.haataja@gmail.com>
2019-02-14 09:50:25 -0600

    Did work on 230

--- csci230/hw5-GrantHaataja.c
+++ csci230/hw5-GrantHaataja.c
@@ -6,7 +6,7 @@
 #include <string.h>
 
 //Structure
-struct data {                                 
+struct _data {                                 
 	char *name;
 	long number;
 };
@@ -37,39 +37,53 @@ int SCAN(char *fileName, FILE *(*stream)) {
 	else {
 		while ((read = getline(&line, &length, *stream)) != -1) {
 			size++;
+			//free(line); //TODO WHY??
 		}
 		return size;
 	}
 }
 
 //function to create a dynamic array of length 'size' and store data in struct
-struct data *LOAD(FILE *stream, int size) {
-	char *line = NULL;
+struct _data *LOAD(FILE *stream, int size) {
+	char *line = NULL; //hold each individual line of file
 	size_t length = 0;
 	ssize_t read = 0;
-	struct data *BlackBox;
-	BlackBox = calloc(size, sizeof(char)); //FIXME
+	struct _data *BlackBox;
+	BlackBox = calloc(size, sizeof(struct _data)); //FIXME ??
+	if (BlackBox == NULL) printf("\nFAIL\n"); //FIXME delete
+	if (BlackBox != NULL) printf("\nSUCCESS\n"); //FIXME delete
 	//rewind stream to start at the beginning of file
 	rewind(stream);
 	for (int i = 0; i < size; i++) {
-		//read = getline(&line, &length, stream);
-		fscanf(stream, "%s %f", BlackBox[i].name, &BlackBox[i].number);
-		//BlackBox[i] = line;
-		printf("\n%s %f\n", BlackBox[i].name, BlackBox[i].number);
-		//TODO fix code to store data properly in structure
+		read = getline(&line, &length, stream);
+		line = strtok(line, " ,\n");
+		BlackBox[i].name = line;
+		line = strtok(NULL, " ");
+		BlackBox[i].number = atol(line);
+		printf("%s %ld\n", BlackBox[i].name, BlackBox[i].number); //FIXME delete
 	}
-
 	return BlackBox;
 }
 
 //function to find the name we are searching for 
-void SEARCH(struct data *BlackBox, char *name, int size) {
-
-
+void SEARCH(struct _data *BlackBox, char *name, int size) {
+	//boolean variable to check if name was not found for case 2
+	int found = 0;
+	//loop through data stored in BlackBox until a match is found
+	for (int i = 0; i < size; i++) {
+		if (strcmp(BlackBox[i].name, name) == 0) {
+			//print the found name for case 3
+			printf("\nThe name was found at the %d entry.", i+1);
+			found = 1;
+		}
+	}
+	if (found == 0) {
+		printf("\nThe name was NOT found.");
+	}
 }
 
 //function to free all dynamic memory allocated in this program
-void FREE(struct data *BlackBox, int size) {
+void FREE(struct _data *BlackBox, int size) {
 
 
 }
@@ -80,7 +94,7 @@ int main(int argc, char **argv) {
 	char *fileName;
 	//fileName = "hw5.data";
 	int size = 0; //number of lines in file
-	struct data *BlackBox;
+	struct _data *BlackBox;
 	//declare file/stream
 	FILE *fstream;
 	//get user-entered file name and dynamically allocate memory for fileName
@@ -109,10 +123,34 @@ int main(int argc, char **argv) {
 			break;
 		}	
 	}
-	printf("File is %d lines.", size);
+	printf("File is %d lines.", size); //FIXME delete
 
 	//call function to store data in structure
 	BlackBox = LOAD(fstream, size);
+	
+	//allocate memory for argv
+	//argv = calloc(size, sizeof(int));
+	printf("%s",argv);
+	printf("\nargc is %d\n",argc);
+	char *name;
+	//name = calloc(size, sizeof(char));
+	//name = *argv;
+	//printf("name is %s\n",name);
+	
+	//make sure user entered a name
+	if (argc <= 1) {
+		printf("You must include a name to search for.\n");
+		exit(0);
+	}
+	//loop through user-entered names and store them in an array
+	for (int i = 0; i < argc-1; i++) {
+		printf("\nI'm in the loop\n");
+		//name[i] = calloc(
+		name[i] = *argv[i+1];
+		printf("entered name: %s\n",name[i]);
+	}
+	//call function to search for user-entered name
+	//SEARCH(BlackBox, name, size);
 
 	printf("\nProgram Complete\n"); //FIXME delete
 
