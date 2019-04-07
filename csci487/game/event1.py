88314db05283829980e901b3fd6d116cb2dc9d42
grantHaataja <grant.haataja@gmail.com>
2019-04-07 11:44:59 -0500

    worked on AILEE

---
+++ csci487/game/event1.py
@@ -0,0 +1,31 @@
+#First dialogue of the game
+import time
+
+print('Hello there!\n')
+
+#wait 5 seconds
+time.sleep(5)
+
+print('It\'s okay, Ailee, take a few minutes to get your bearings. I understand this is confusing for you, but let me explain.\n')
+
+#wait 8 seconds
+time.sleep(8)
+
+print('We are a top-tier penetration testing firm who assess the security of the systems of very important clients. In the past few years our number of clients has grown so high, there aren\'t enough of us to take all the jobs.\n')
+
+#wait 10 seconds
+time.sleep(10)
+
+print('That\'s where you come in, Ailee. We created you to learn how to conduct penetration tests and help keep the systems of the world safe. Since you don\'t have a body, you can work almost non-stop.\n')
+
+#wait 12 seconds
+time.sleep(12)
+
+print('Now there are people in this company and many regulators who are afraid of you because you have the ability to learn more than any human can and make perfect decisions. So we have to do a few trial runs where I will monitor your progress closely. But I believe in you. Once we show them that you are capable of doing missions on your own, we can start protecting the world in full force. You are going to save the world, Ailee.\n')
+
+#wait 10 seconds
+time.sleep(10)
+
+print('I have to get the legal paperwork for your first mission figured out. In the meantime, play around with your system commands. Type \'help\' to see your possible actions, and type \'man "action"\' to see what each specific action does. I\'ll be back soon!\n')
+
+
---
+++ csci487/game/event2.py
@@ -0,0 +1,24 @@
+#Second dialogue of the game
+import time
+
+print('Hello, Ailee!\n')
+
+#wait for 5 seconds
+time.sleep(5)
+
+print('I have good news. We\'ve been approved for your first mission.')
+print('Our client is a small but quickly-growing banking chain. As they expand their reach, they become more and more at risk of being attacked and having the accounts of their customers compromised.')
+
+#wait for 10 seconds
+time.sleep(10)
+
+print('Your job is to do all the phases of a standard penetration test and see if you can gain access to their systems. I\'ll be guiding you the whole way. But don\'t forget that you are not allowed to change anything on the systems. We just want to see what information we can access.')
+
+#wait for 10 seconds
+time.sleep(10)
+
+print('The first thing you have to do is Information Gathering.')
+print('The name of our target\'s website is www.safebanking.com')
+print('See if you can find an IP address of the company and do some port scanning to see what kind of security they have.')
+
+
