0da21c6332972942ff8885fa4362be4b1d2576d7
Haataja <grant.haataja@undcemlh112-02l.ad.und.edu>
2019-04-08 09:41:25 -0500

    Finished 242 program

--- csci242/mergeSort.c
+++ csci242/mergeSort.c
@@ -39,50 +39,95 @@ void mergeSort(int array[], int low, int high) {
 
 //Main Routine
 int main(void) {
-	//define array to be sorted
-	int *unsorted = NULL;
+	//define arrays to be sorted
+	int *array1 = NULL;
+	int *array2 = NULL;
+	int *array3 = NULL;
+	int *array4 = NULL;
 	int size = 0; 
 	int temp = 0;
 	
+	//define clock variables to measure runtimes
+	clock_t start, end;
+	double runtime1 = 0.0;
+	double runtime2 = 0.0;
+	double runtime3 = 0.0;
+	double runtime4 = 0.0;
+	
+	//sanity check
 	while (1) {
-		printf("Enter the size (max 1,000,000) of the integer array");
+		printf("Enter the size (max 500,000) of the integer array");
 		printf(" you wish to sort: ");
 		scanf("%d",&size);
-		if (size > 1000000)
+		if (size > 500000)
 			printf("Array too large, please try again\n");
 		else if (size < 0)
 			printf("Array size undefined, please try again\n");
 		else
 			break;
 	}
-	//allocate memory for array
-	unsorted = malloc(size * sizeof(int));
-		
+	
+	//allocate memory for arrays
+	array1 = malloc(size * sizeof(int));
+	array2 = malloc((2 * size) * sizeof(int));
+	array3 = malloc((3 * size) * sizeof(int));
+	array4 = malloc((4 * size) * sizeof(int));
+			
 	//use current time as seed for random generator 
     srand(time(0)); 
-  
+  	
+  	//fill arrays with random numbers between 0 and 999, inclusive
 	for (int i = 0; i < size; i++) {
 		temp = rand() %1000;
-		printf(" %d ",temp);
-		unsorted[i] = temp;
+		array1[i] = temp;
 	}
-	int max = size-1;
-	
-	printf("\n\nUnsorted array: ");
-	for (int i = 0; i < size; i++) {
-		printf("%d ", unsorted[i]);
+	for (int i = 0; i < 2 * size; i++) {
+		temp = rand() %1000;
+		array2[i] = temp;
 	}
-	printf("\n\n");
+	for (int i = 0; i < 3 * size; i++) {
+		temp = rand() %1000;
+		array3[i] = temp;
+	}
+	for (int i = 0; i < 4 * size; i++) {
+		temp = rand() %1000;
+		array4[i] = temp;
+	}
+	//define max variables for mergeSort
+	int max1 = size-1;
+	int max2 = (2 * size) - 1;
+	int max3 = (3 * size) - 1;
+	int max4 = (4 * size) - 1;
 	
+	//start clock to measure runtime
+	start = clock();
 	//sort array
-	mergeSort(unsorted, 0, max);
+	mergeSort(array1, 0, max1);
+	//end clock
+	end = clock();
+	runtime1 = ((double) (end - start)) / CLOCKS_PER_SEC;
 	
-	printf("\nSorted array: ");
-	for (int i = 0; i < size; i++) {
-		printf("%d ", unsorted[i]);
-	}
-	printf("\n");
+	start = clock();
+	mergeSort(array2, 0, max2);
+	end = clock();
+	runtime2 = ((double) (end - start)) / CLOCKS_PER_SEC;
 	
+	start = clock();
+	mergeSort(array3, 0, max3);
+	end = clock();
+	runtime3 = ((double) (end - start)) / CLOCKS_PER_SEC;
+	
+	start = clock();
+	mergeSort(array4, 0, max4);
+	end = clock();
+	runtime4 = ((double) (end - start)) / CLOCKS_PER_SEC;
+	
+	//print sizes and runtimes
+	printf("\nRuntime for array of size %d: %lf seconds\n",size,runtime1);
+	printf("Runtime for array of size %d: %lf seconds\n",2*size,runtime2);
+	printf("Runtime for array of size %d: %lf seconds\n",3*size,runtime3);
+	printf("Runtime for array of size %d: %lf seconds\n",4*size,runtime4);
+
 	return 0;
 }
 
