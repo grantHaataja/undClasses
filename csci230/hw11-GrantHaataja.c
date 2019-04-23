3dcdeb1a68d2bd237929bd5be43b133f9f6cfd6c
Grant Haataja <grant.haataja@gmail.com>
2019-04-23 15:17:04 -0500

    Loaded all destinations into linked list

--- csci230/hw11-GrantHaataja.c
+++ csci230/hw11-GrantHaataja.c
@@ -5,6 +5,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct node {
+	char *word;
+	int index;
+	struct node *next;
+};
+
 //function to count lines in file
 int countLines(char *fileName, FILE *(*stream)) {
 	//variable for number of lines
@@ -30,7 +36,52 @@ int countLines(char *fileName, FILE *(*stream)) {
 	}
 }
 
-//function to populate linked
+//function to populate linked list with the establishments in the list
+struct node* populate(struct node *head, struct node *current, int size, 
+		FILE *stream) {
+	//rewind the file to start at the beginning
+	rewind(stream);
+	//string variable to hold each individual line
+	char *line = NULL;
+	size_t length = 0;
+	//to store number of characters in each line	
+	char *word;
+	//allocate memory for first node
+	head = (struct node*) malloc(sizeof(struct node));
+	current = head;
+	int i = 0;
+	//mosey through each line of the file
+	for (i = 0; i < size; i++) {
+		getline(&line, &length, stream);
+		//get the string token for the first word of the line
+		word = strtok(line, "\n");
+		//check to see if the end of the establishments is here
+		if (strcmp(word, "STOP") == 0) {
+			break;
+		}
+		//allocate memory for the word data for each node
+		current->word = (char *) calloc(strlen(word)+1, sizeof(char));
+		//assign the word of current node with the establishment
+		strncpy(current->word, word, strlen(word));
+		//assign node its index number
+		current->index = i;
+		//allocate memory for next current node			
+		current->next = (struct node*) malloc(sizeof(struct node));
+		current = current->next;
+		//hide the body
+		free(line);
+		line = NULL;
+	}
+	return head;
+}
+
+//function to print and display to the screen 
+void display(struct node *current) {
+	while (current) {
+		printf("Node %d: %s\n",current->index, current->word);
+		current = current->next;
+	}
+}
 
 
 //Main Routine
@@ -39,10 +90,17 @@ int main(void) {
 	char *fileName = "hw111.data";
 	FILE *stream;
 	int size = 0;
+	struct node *head = NULL;
+	struct node *current = NULL;
 	
 	//count how many lines in file
 	size = countLines(fileName, &stream);
 	printf("There are %d lines in file\n",size);
-
+	
+	//populate linked list with each establishment from the input file
+	head = populate(head, current, size, stream);
+	current = head;
+	
+	display(head);//FIXME
 
 }
