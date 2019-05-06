b1e97e5335fcaeb09a143c15894124035586303f
Misha Turnbull <mishaturnbull@gmail.com>
2019-05-05 21:39:34 -0500

    Switch back to old code
    Signed-off-by: Misha Turnbull <mishaturnbull@gmail.com>

--- csci487/project/filesystem.py
+++ csci487/project/filesystem.py
@@ -1,205 +1,68 @@
-import hashlib
+import termcolor
+
+class Directory:
+  '''
+    Tree structure of directories and files
+  '''
+  def __init__(self, name=None, parent=None, children=None):
+    self.name = name or ''
+    self.parent = parent or self # So root node points to itself as parent
+    self.children = {
+      '.':self,
+      '..':self.parent
+    }
+    if type(children) is dict:
+      self.children.update(children)
+
+  def mkdir(self, name):
+    assert (name not in self.children), 'Directory already exists'
+    newDir = Directory(name, self)
+    self.children.update({name:newDir})
+    return newDir
+
+  def addFile(self, fileName, fileContents):
+    assert (fileName not in self.children), 'File already exists'
+    assert ('.' in fileName), 'File has no type'
+    newFile = File(fileName, fileContents)
+    self.children.update({fileName:newFile})
+    return newFile
+  
+  def rmFile(self, fileName):
+    assert (fileName in self.children), "File does not exist"
+    assert ('.' in fileName), "File has no type"
+    del self.children[fileName]
+
+  def __iter__(self):
+    return (fName for fName in self.children)
+    
+  def __repr__(self, base=True):
+    return (self.parent.__repr__(base=False) + '/' if self.parent.name else '') + self.name + ('/' if base else '')
+  __str__ = __repr__ # set __str__ as the same method as __repr__
+  
+  def __getitem__(self, item):
+    assert (item in self.children), "File or Directory not found"
+    return self.children[item]
+
+  def __len__(self):
+    return len(self.children)
 
 
-class Permissions(object):
-    """
-    rwxrwx
-    |||||\ Owner exec
-    ||||\ Owner write
-    |||\ Owner read
-    ||\ General user exec
-    |\ General user write
-    \ General user read
-    """
+class File:
+  '''
+    Stores things. Like data, machine code, and blackmail
+  '''
+  def __init__(self, name, data='', permissions='rw-rw-', owner=None):
+    self.name = name
+    self.data = data
+    self.permissions = permissions
+    self.owner = owner
 
-    def __init__(self, perms):
-        self._bits = perms
-        assert len(self._bits) == 6
+  def append(self, data):
+    self.data += data
 
-    def _set_bit(self, n, newval):
-        bits = list(self._bits)
-        bits[n] = newval
-        self._bits = ''.join(bits)
+  def __repr__(self):
+    return self.name
+  __str__ = __repr__ # set __str__ as the same method as __repr__
 
-    def __str__(self):
-        return self._bits
-
-    def __eq__(self, other):
-        if isinstance(other, Permissions):
-            return self._bits == other._bits
-        elif isinstance(other, str):
-            return self._bits == other
-        else:
-            return False
-
-    def __hash__(self):
-        return hash(self._bits)
-
-    def __getitem__(self, bit):
-        return self._bits[bit]
-
-    def __setitem__(self, bit, newval):
-        self._set_bit(bit, newval)
-
-    @property
-    def read_users(self):
-        return self._bits[0] == 'r'
-
-    @read_users.setter
-    def read_users(self, allowed):
-        self._set_bit(0, 'r' if allowed else '-')
-
-    @property
-    def write_users(self):
-        return self._bits[1] == 'w'
-
-    @write_users.setter
-    def write_users(self, allowed):
-        self._set_bit(1, 'w' if allowed else '-')
-
-    @property
-    def exec_users(self):
-        return self._bits[2] == 'x'
-
-    @exec_users.setter
-    def exec_users(self, allowed):
-        self._set_bit(2, 'x' if allowed else '-')
-
-    @property
-    def read_owner(self):
-        return self._bits[3] == 'r'
-
-    @read_owner.setter
-    def read_owner(self, allowed):
-        self._set_bit(3, 'r' if allowed else '-')
-
-    @property
-    def write_owner(self):
-        return self._bits[4] == 'w'
-
-    @write_owner.setter
-    def write_owner(self, allowed):
-        self._set_bit(4, 'w' if allowed else '-')
-
-    @property
-    def exec_owner(self):
-        return self._bits[5] == 'x'
-
-    @exec_owner.setter
-    def exec_owner(self, allowed):
-        self._set_bit(5, 'x' if allowed else '-')
-
-
-class Directory(object):
-    """
-      Tree structure of directories and files
-    """
-    def __init__(self, name=None, parent=None, children=None, permissions='r-xr-x',
-                 owner=None):
-        self.name = name or ''
-        if parent is None:
-            self.parent = self
-        else:
-            self.parent = parent
-        self.permissions = Permissions(permissions)
-        self.owner = owner or 'n/a'
-        self.children = {
-            '.': self,
-            '..': self.parent
-        }
-        if type(children) is dict:
-            self.children.update(children)
-
-    def mkdir(self, name, **kwargs):
-        assert (name not in self.children), 'Directory already exists'
-        newDir = Directory(name=name, parent=self, **kwargs)
-        self.children.update({name: newDir})
-        return newDir
-
-    def addFile(self, fileName, fileContents, **kwargs):
-        assert (fileName not in self.children), 'File already exists'
-        assert ('.' in fileName), 'File has no type'
-        newFile = File(fileName, fileContents, **kwargs)
-        self.children.update({fileName: newFile})
-        return newFile
-
-    def addPrebuiltFile(self, file, **kwargs):
-        assert (file.name not in self.children), "File already exists"
-        self.children.update({file.name: file})
-        return file
-
-    def rmFile(self, fileName):
-        assert (fileName in self.children), "File does not exist"
-        assert ('.' in fileName), "File has no type"
-        del self.children[fileName]
-
-    def __iter__(self):
-        return (fName for fName in self.children)
-
-    def __repr__(self, base=True):
-        output = ""
-        if self.parent is not self:
-            up1 = self.parent
-            while isinstance(up1, Directory):
-                output = up1.name + "/" + output
-                if up1 is up1.parent:
-                    break
-                else:
-                    up1 = up1.parent
-        output += self.name
-        if base:
-            output += '/'
-        return output
-
-    __str__ = __repr__  # set __str__ as the same method as __repr__
-
-    def __getitem__(self, item):
-        assert (item in self.children), "File or Directory not found"
-        return self.children[item]
-
-    def __len__(self):
-        # Remove the "." and ".." folders from the size
-        # They're not really there
-        return len(self.children) - 2
-
-
-class File(object):
-    '''
-      Stores things. Like data, machine code, and blackmail
-    '''
-    def __init__(self, name, data='', permissions='r--r--', owner=None, **kwargs):
-        self.name = name
-        self._data = data
-        self.permissions = Permissions(permissions)
-        self.owner = owner or 'n/a'
-        self._original_hash = hashlib.md5(data.encode('utf-8')).hexdigest()
-        self._current_hash = hashlib.md5(data.encode('utf-8')).hexdigest()
-        self._kwargs = kwargs
-
-    def append(self, data):
-        self._data += data
-        self._current_hash = hashlib.md5(self._data.encode('utf-8')).hexdigest()
-
-    @property
-    def original_hash(self):
-        return self._original_hash
-
-    @property
-    def current_hash(self):
-        return self._current_hash
-
-    @property
-    def data(self):
-        return self._data
-
-    @data.setter
-    def data(self, newdata):
-        self._data = newdata
-        self._current_hash = hashlib.md5(self._data.encode('utf-8')).hexdigest()
-
-    def __repr__(self):
-        return self.name
-    __str__ = __repr__ # set __str__ as the same method as __repr__
-
-    def __len__(self):
-        return len(self._data)
+  def __len__(self):
+    return len(self.data)
--- csci487/project/shell.py
+++ csci487/project/shell.py
@@ -1,12 +1,8 @@
-# -*- coding: utf-8 -*-
-
 from termcolor import colored
-import funfunctions
+import replit
 
 import time
-import traceback
-import sys
-import random
+import traceback, sys, random
 
 import executables
 import events
@@ -14,133 +10,139 @@ from MainMenuException import MainMenuException
 
 DEFAULT_PROMPT = colored("AILEE@{COMP}: {CWD}$ ", 'green')
 
+CMD_NOT_FOUND_STRS = [
+  "command not found"
+]
 
 class Shell(object):
+  """
+  Like a seashell.
+  """
+
+  def __init__(self, computer, user, agent=None, cwd=None, game=None):
     """
-    Like a seashell.
+    Create a shell.
     """
 
-    def __init__(self, computer, user, agent=None, cwd=None, game=None):
-        """
-        Create a shell.
-        """
+    self.computer = computer
+    self.user = user
+    self.agent = agent
+    self.cwd = cwd or computer.fs
+    self.prompt = DEFAULT_PROMPT
+    self.running = False
+    self._command_dictionary = {}
+    self.variables = {}
+    self.game = game
+    self.history = []
 
-        self.computer = computer
-        self.user = user
-        self.agent = agent
-        self.cwd = cwd or computer.fs
-        self.prompt = DEFAULT_PROMPT
-        self.running = False
-        self._command_dictionary = {}
-        self.variables = {}
-        self.game = game
-        self.history = []
+    self._setup()
+  
+  def _setup(self):
+    replit.clear()
+    s = "Loading new shell"
 
-        self._setup()
+    print(s, end='\r')
+    i = 0
 
-    def _setup(self):
-        funfunctions.clear()
-        s = "Loading new shell"
+    # load command dictionary
+    for module in executables.__all__:
+      self._command_dictionary.update({module: getattr(executables, module).run})
+      print(s + '.'*i, end='\r')
+      i += 1
+      time.sleep(0.1)
+    time.sleep(0.3)
+    replit.clear()
+    #print(constants.title)
+  
+  def _get_command_from_str(self, command_str):
+    """
+    Takes a command name, returns the executable object.
+    """
+    
+    if command_str == '':
+      return False
+    if command_str not in self.game.allowed_commands:
+      return None
+    cmd = self._command_dictionary[command_str]
+    return cmd
+  
+  def run_command(self, command, args):
+    """
+    Runs a command.
 
-        print(s, end='\r')
-        i = 0
+    Input must be a runnable command that accepts **kwargs.
+    """
 
-        # load command dictionary
-        for module in executables.__all__:
-            self._command_dictionary.update({module: getattr(executables, module).run})
-            print(s + '.'*i, end='\r')
-            i += 1
-            time.sleep(0.1)
-        time.sleep(0.3)
-        funfunctions.clear()
-        # print(constants.title)
+    command(
+      *args,
+      computer=self.computer,
+      cwd=self.cwd,
+      user=self.user,
+      agent=self.agent,
+      shell=self,
+      game=self.game,
+    )
+  
+  def take_input(self):
+    user_input = input(self.prompt.format(
+      COMP=str(self.computer.name),
+      CWD=str(self.cwd), 
+      USER=self.user),
+    )
 
-    def _get_command_from_str(self, command_str):
-        """
-        Takes a command name, returns the executable object.
-        """
+    parts = [p.strip() for p in user_input.split(' ')]
+    command = parts[0]
+    args = parts[1:]
 
-        if command_str == '':
-            return False
-        if command_str not in self.game.allowed_commands:
-            return None
-        cmd = self._command_dictionary[command_str]
-        return cmd
+    return command, args
 
-    def run_command(self, command, args, **kwargs):
-        """
-        Runs a command.
+  def one_command(self):
+    command, args = self.take_input()
+    cmd = self._get_command_from_str(command)
+    if cmd is None:
+      self.cmd_not_found()
+      return
+    elif cmd is False:
+      return  # nothing on empty commands
+    cname = cmd.__module__.split('.')[-1]
+    if not cname == 'doStory':
+      self.game.history.append([cname, args])
+      self.history.append([cname, args])
 
-        Input must be a runnable command that accepts **kwargs.
-        """
+    if not (command or args):
+      return  # skip empty input
+    self.run_command(cmd, args)
+  
+  def halt(self):
+    self.running = False
+  
+  def cmd_not_found(self):
+    self.history.append([None, []])
+    self.game.history.append([None, []])
+    print("Command not found")
+    #print(random.choice(CMD_NOT_FOUND_STRS))
+  
+  def start_shell_loop(self):
+    self.running = True
+    while self.running:
 
-        command(
-            *args,
-            computer=self.computer,
-            cwd=self.cwd,
-            user=self.user,
-            agent=self.agent,
-            shell=self,
-            game=self.game,
-            **kwargs
-        )
+      # This is the line of code that integrates the story VVV
+      
 
-    def take_input(self):
-        user_input = input(self.prompt.format(
-            COMP=str(self.computer.name),
-            CWD=str(self.cwd),
-            USER=self.user),
-        )
-
-        parts = [p.strip() for p in user_input.split(' ')]
-        command = parts[0]
-        args = parts[1:]
-
-        return command, args
-
-    def one_command(self):
-        command, args = self.take_input()
-        cmd = self._get_command_from_str(command)
-        if cmd is None:
-            self.cmd_not_found()
-            return
-        elif cmd is False:
-            return  # nothing on empty commands
-        cname = cmd.__module__.split('.')[-1]
-        if not cname == 'doStory':
-            self.game.history.append([cname, args])
-            self.history.append([cname, args])
-
-        if not (command or args):
-            return  # skip empty input
-        self.run_command(cmd, args)
-
-    def halt(self):
-        self.running = False
-
-    def cmd_not_found(self):
-        self.history.append([None, []])
-        self.game.history.append([None, []])
-        print("Command not found")
-
-    def start_shell_loop(self):
-        self.running = True
-        while self.running:
-            try:
-                self.run_command(events.doStory.run, [])
-                self.one_command()
-            except KeyboardInterrupt:
-                print()
-            #except KeyError as e:
-            #    self.cmd_not_found()
-            except AssertionError as e:
-                print(str(e))
-            except MainMenuException:
-                raise MainMenuException
-            except Exception as e:
-                #print(colored(
-                #    "Something went wrong.  I'm not quite sure what.  "
-                #    "Maybe try again?", 'red'))
-                # Uncomment VV for full tracebacks
-                einfo = sys.exc_info()
-                traceback.print_exception(*einfo)
+      try:
+        self.run_command(events.doStory.run, [])
+        self.one_command()
+      except KeyboardInterrupt:
+        print()
+        #print("\nYou can't leave! ", end='')
+      except KeyError as e:
+        self.cmd_not_found()
+      except AssertionError as e:
+        print(str(e))
+      except MainMenuException:
+        raise MainMenuException
+      except Exception as e:
+        print(colored("Something went wrong.  I'm not quite sure what.  Maybe try again?", 'red'))
+        # Uncomment VV for full tracebacks
+        #einfo = sys.exc_info()
+        #traceback.print_exception(*einfo)
