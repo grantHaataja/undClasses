8463892299c7a95974e7059f0bfa468a7e6d1dbb
Grant <grant@nsquaredmedia.com>
2020-02-17 23:43:40 -0600

    Did almost everything for 365 Assignment
    Signed-off-by: Grant <grant@nsquaredmedia.com>

--- csci365/Program2/Program2.java
+++ csci365/Program2/Program2.java
@@ -14,12 +14,45 @@ public class Program2
 			System.exit(0);
 		}
 		String fileName = args[0];
+		String[] reserved = new String[]{"input", "print", "begin", "end", "if", "else"};
+		String[] relOp = new String[]{"<", ">", "<=", ">=", "==", "!="};
 		
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));     
 			String fileLine = null;
-            while((fileLine = br.readLine()) != null)
-                System.out.println(fileLine);
+			//this is where we begin parsing the file content
+            while((fileLine = br.readLine()) != null) {
+                //fileLine = br.readLine();
+				if (fileLine.charAt(0) == '#') {
+					continue;
+				}
+				//split the file line into individual tokens
+				String[] splited = fileLine.split("\\s+");
+				//now loop through the line to analyze tokens one at a time
+				for (int i = 0; i < splited.length; i++) {
+					//see if the token is a reserved word
+					for (int j = 0; j < reserved.length; j++) {
+						if (splited[i].equals(reserved[j])) {
+							System.out.println("<" + reserved[j] + ">, " + reserved[j]);
+						}
+					}
+					//see if token is the assign token
+					if (splited[i].equals("=")) {
+						System.out.println("<assign>, " + splited[i]);
+					}
+					//see if token is a relational operator
+					for (int k = 0; k < relOp.length; k++) {
+						if (splited[i].equals(relOp[k])) {
+							System.out.println("<rel_op>, " + relOp[k]);
+						}
+					}
+					//see if token is a number
+					if (splited[i].matches("(\\d+)(\\.\\d+)?")) {
+						System.out.println("<number>, " + splited[i]);
+					}
+				}
+								
+			}
 		} catch (IOException e) {
 			System.err.println(e);
 		}
