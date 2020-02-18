f9d70fe1843635915cc6f07a9f7aa1135b1e61e9
Grant <grant@nsquaredmedia.com>
2020-02-17 19:29:09 -0600

    Got File IO working on 365 Program2
    Signed-off-by: Grant <grant@nsquaredmedia.com>

--- csci365/Program2/Program2.java
+++ csci365/Program2/Program2.java
@@ -1,25 +1,28 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.FileNotFoundException;
 
-public class Program2 {
-	public static void main(String[] args) throws IOException {
+public class Program2 
+{
+	public static void main(String[] args) throws IOException 
+	{
+		if (args.length != 1) {
+			System.out.println("Improper parameters. Usage: java Program2 [fileName]");
+			System.exit(0);
+		}
 		String fileName = args[0];
 		
 		try {
-			File f = new File(fileName);     
-			System.out.println("File name: " + f.getName());
-			System.out.println("Is writeable: " + f.canWrite());
-			System.out.println("Is readable: " + f.canRead());
-			System.out.println("Is a directory: " + f.isDirectory());
-			System.out.println("File size in bytes: " + f.length());
-		} catch (FileNotFoundException e) {
+			BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));     
+			String fileLine = null;
+            while((fileLine = br.readLine()) != null)
+                System.out.println(fileLine);
+		} catch (IOException e) {
 			System.err.println(e);
 		}
-	
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 
 	}
