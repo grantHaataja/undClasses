08846e22d1e72784e0fa264b892ff520534ed049
Haataja <grant.haataja@undcemlh110-07l.ad.und.edu>
2020-02-18 14:44:48 -0600

    Cleanup
    Signed-off-by: Haataja <grant.haataja@undcemlh110-07l.ad.und.edu>

---
+++ csci364/hw2/src/Driver.java
@@ -0,0 +1,75 @@
+//CSCI364 Assignment II
+//Grant Haataja
+import java.lang.Runnable;
+import java.lang.InterruptedException;
+
+public class Driver
+{
+	public static void main(String[] args)
+	{
+		//verify user-entered arguments
+		if ((args.length != 4) || Integer.parseInt(args[0]) < 1 || 
+				Integer.parseInt(args[1]) < 1 || Integer.parseInt(args[2]) <= 0) {
+			System.err.println("Usage: java -cp build Driver [number of deposit threads]"
+				+ " [number of withdrawal threads] [inital account balance] " +
+				"[milliseconds main thread must sleep before and after changing threads]");
+			System.exit(0);
+		}
+		//define variables for command line arguments
+		int depositThreads = Integer.parseInt(args[0]);
+		int withdrawalThreads = Integer.parseInt(args[1]);
+		int accountBalance = Integer.parseInt(args[2]);
+		int sleepyTime = Integer.parseInt(args[3]);
+		
+		//create the account
+		Account myAcc = new Account(accountBalance);
+		
+		//start the deposit threads
+		Thread[] dThreads = new Thread[depositThreads];
+		for (int i = 0; i < depositThreads; i++) {
+			DepositRunnable dRun = 
+				new DepositRunnable(myAcc, i+1, accountBalance);
+			dThreads[i] = new Thread(dRun);
+			dThreads[i].start();
+		}
+		
+		//start the withdrawal threads
+		Thread[] wThreads = new Thread[withdrawalThreads];
+		for (int i = 0; i < withdrawalThreads; i++) {
+			WithdrawalRunnable wRun = new WithdrawalRunnable(myAcc, i+1);
+			wThreads[i] = new Thread(wRun);
+			wThreads[i].start();
+		}
+		//sleep for the appropriate time
+		try {
+			Thread.sleep(sleepyTime);
+		} catch (InterruptedException e) {
+			System.err.println("Main thread interrupted.\n");
+			System.exit(0);
+		}
+		System.out.println();
+		
+		//interrupt the deposit threads
+		for (int i = 0; i < depositThreads; i++) {
+			dThreads[i].interrupt();
+			try {
+				dThreads[i].join();
+			} catch (InterruptedException e) {
+				System.err.println(e);
+			}
+		}
+		
+		//interrupt the withdrawal threads
+		for (int i = 0; i < withdrawalThreads; i++) {
+			wThreads[i].interrupt();
+			try {
+				wThreads[i].join();
+			} catch (InterruptedException e) {
+				System.err.println(e);
+			}
+		}
+		
+		//print the final account balance
+		System.out.println("Final account balance: $" + myAcc.getAccountBalance());
+	}
+}
