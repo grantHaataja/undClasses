85a23979b5eb74db53a949a2b367b9eaa9825746
grantHaataja <grant.haataja@gmail.com>
2020-08-28 11:47:25 -0500

    Did hw1 for 451
    Signed-off-by: grantHaataja <grant.haataja@gmail.com>

---
+++ csci451/hw1-granthaataja.c
@@ -0,0 +1,15 @@
+//Grant Haataja - Hw 1
+
+#include <stdlib.h>
+
+int main() 
+{
+	//first make the system call to wget
+	system("wget -q http://www.senate.gov/general/contact_information/senators_cfm.cfm");
+	
+	//next display the web addresses on the screen
+	system("grep -Eoi '<a [^>]+>' senators_cfm.cfm | grep -Eo 'href=\"[^\\\"]+\"' | grep -Eo '(http|https):\/\/[^\/\"]+' ");
+	
+	//finally unlink the downloaded file
+	system("unlink senators_cfm.cfm");
+}
