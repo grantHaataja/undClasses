31de4d35b56449a55d65b29c85df90680b94c959
Haataja <grant.haataja@undcemlh112-29l.cem.und.edu>
2020-09-02 11:38:39 -0500

    Finalized hw1
    Signed-off-by: Haataja <grant.haataja@undcemlh112-29l.cem.und.edu>

--- csci451/hw1-granthaataja.c
+++ csci451/hw1-granthaataja.c
@@ -8,7 +8,7 @@ int main()
 	system("wget -q http://www.senate.gov/general/contact_information/senators_cfm.cfm");
 	
 	//next display the web addresses on the screen
-	system("grep -Eoi '<a [^>]+>' senators_cfm.cfm | grep -Eo 'href=\"[^\\\"]+\"' | grep -Eo '(http|https):\/\/[^\/\"]+' ");
+	system("grep -Eoi '<a [^>]+>' senators_cfm.cfm | grep -Eo 'href=\"[^\\\"]+\"' | grep -Eo '(http|https):\/\/[^\"]+' ");
 	
 	//finally unlink the downloaded file
 	system("unlink senators_cfm.cfm");
