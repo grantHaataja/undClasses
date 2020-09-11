a611c2d00f768b2e4e2070e7ebfd67968cd52362
grantHaataja <grant.haataja@gmail.com>
2020-09-11 12:10:49 -0500

    Finalized hw2
    Signed-off-by: grantHaataja <grant.haataja@gmail.com>

--- csci451/hw2-granthaataja.c
+++ csci451/hw2-granthaataja.c
@@ -2,14 +2,28 @@
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 int main(int argc, char** argv)
 {
+	//store the name of the executable as an argument to pass to execvp
 	char *args[] = {argv[0], NULL};
+	int exitStatus = 1;
+	pid_t PID;
 	
-	fork();
-	system("firefox");
-	fork();
-	execvp(argv[0], args);
+	//create child process
+	PID = fork();
+	//check if the child process is returned and then execute the child
+	if (PID == 0) {
+		execvp(argv[0], args);
+		exit(exitStatus);	
+	}
+	//start a program to further increase the destruction of the system
+	else {
+		system("gnome-terminal");
+		waitpid(PID, &exitStatus, 0);
+	}
 
+	return 0;
 }
